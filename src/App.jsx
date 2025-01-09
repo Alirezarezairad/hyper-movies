face7de0aade6038b729262352e7c376d1ade095
alireza <alirezarezairad5@gmail.com>
2025-01-09 14:43:54 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/header";
 
 function App() {
